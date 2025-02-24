5e182a15a7685b9705e975c8fc5e489c0ec908a2
baiching <bachmarma@gmail.com>
2025-02-24 20:22:22 +0100

    action for user registration

---
+++ react-redux-hooks-jwt-auth/src/actions/auth.js
@@ -0,0 +1,52 @@
+/*
+* register/login/logout actions
+ */
+
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
+    SET_MESSAGE
+} from "./type";
+
+import AuthService from "../services/auth.service";
+
+export const register = (username, email, password) => (dispatch) => {
+    return AuthService.register(username, email, password)
+        .then((response) => {
+            dispatch ({
+                type: REGISTER_SUCCESS,
+            });
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: response.data.message,
+            });
+
+            return Promise.resolve();
+        },
+            (error) => {
+            const message =
+                (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+                error.message ||
+                error.toString();
+
+            dispatch({
+                type: REGISTER_FAIL
+            });
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: message
+            });
+
+            return Promise.resolve();
+
+            }
+        );
+};
+
