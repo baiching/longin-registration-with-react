63d4f50331bf4a76b173d423a9a94e82745297e1
baiching <bachmarma@gmail.com>
2025-02-24 20:32:50 +0100

    created services for login and logout

--- react-redux-hooks-jwt-auth/src/actions/auth.js
+++ react-redux-hooks-jwt-auth/src/actions/auth.js
@@ -12,6 +12,7 @@ import {
 } from "./type";
 
 import AuthService from "../services/auth.service";
+import {data} from "react-router-dom";
 
 export const register = (username, email, password) => (dispatch) => {
     return AuthService.register(username, email, password)
@@ -44,9 +45,49 @@ export const register = (username, email, password) => (dispatch) => {
                 payload: message
             });
 
-            return Promise.resolve();
+            return Promise.reject();
 
             }
         );
 };
 
+export const login = (username, password) => (dispatch) => {
+    return AuthService.login(username, password).then(
+        (data) => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: {user: data},
+            });
+
+            return Promise.resolve();
+        },
+
+        (error) => {
+            const message =
+                (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+                error.message ||
+                error.toString();
+
+            dispatch({
+                type: LOGIN_FAIL
+            });
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: message
+            });
+
+            return Promise.reject();
+        }
+    );
+};
+
+export const logout = () => (dispatch) => {
+    AuthService.logout();
+
+    dispatch({
+        type: LOGOUT
+    });
+};
